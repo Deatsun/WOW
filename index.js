c9951a15cbf513a1ed21e236528f8bd105bab40d
Molnár Bence <mbbexonait@gmail.com>
2025-09-17 10:25:50 +0200

    im fixed the dungeons card

---
+++ index.js
@@ -0,0 +1,15 @@
+document.getElementById("vaultbtn").addEventListener("click", function(){
+    window.location.href = "https://www.wowhead.com/guide/systems/the-great-vault";
+})
+
+document.getElementById("trinketbtn").addEventListener("click", function(){
+    window.location.href = "https://www.aoeah.com/news/4015--wow-tww-season-3-trinket-tier-list--best-dps-healer-tank-trinkets-112";
+})
+
+document.getElementById("consumbtn").addEventListener("click", function(){
+    window.location.href ="https://www.method.gg/guides/list-of-all-the-war-within-consumables-enchants-and-gems";
+})
+
+document.getElementById("craftbtn").addEventListener("click", function(){
+    window.location.href = "https://www.wowhead.com/news/best-crafted-gear-embellishments-for-all-classes-the-war-within-season-3-378128";
+})
